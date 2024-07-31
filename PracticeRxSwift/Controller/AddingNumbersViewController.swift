1eb2e0bd025bdad0c687587fb6845ffe96aa5e24
yeggrrr <yjkim981103@gmail.com>
2024-07-31 21:13:15 +0900

    [Feat] AddingNumbersVC - 더하기 계산기 구현

--- PracticeRxSwift/Controller/AddingNumbersViewController.swift
+++ PracticeRxSwift/Controller/AddingNumbersViewController.swift
@@ -10,10 +10,89 @@ import RxSwift
 import RxCocoa
 import SnapKit
 
-class AddingNumbersViewController: UIViewController {
+class AddingNumbersViewController: UIViewController, ViewRepresentable {
+    let number1 = UITextField()
+    let number2 = UITextField()
+    let number3 = UITextField()
+    let plusLabel = UILabel()
+    let resultLabel = UILabel()
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addSubviews()
+        setConstraints()
+        configureUI()
+        calculator()
     }
     
+    func addSubviews() {
+        view.addSubviews([number1, number2, number3, plusLabel, resultLabel])
+    }
+    
+    func setConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        number1.snp.makeConstraints {
+            $0.top.equalTo(safeArea).offset(200)
+            $0.leading.equalTo(safeArea).offset(100)
+            $0.trailing.equalTo(safeArea).offset(-80)
+            $0.height.equalTo(30)
+        }
+        
+        number2.snp.makeConstraints {
+            $0.top.equalTo(number1.snp.bottom).offset(10)
+            $0.leading.equalTo(safeArea).offset(100)
+            $0.trailing.equalTo(safeArea).offset(-80)
+            $0.height.equalTo(30)
+        }
+        
+        number3.snp.makeConstraints {
+            $0.top.equalTo(number2.snp.bottom).offset(10)
+            $0.leading.equalTo(safeArea).offset(100)
+            $0.trailing.equalTo(safeArea).offset(-80)
+            $0.height.equalTo(30)
+        }
+        
+        plusLabel.snp.makeConstraints {
+            $0.centerY.equalTo(number3.snp.centerY)
+            $0.trailing.equalTo(number3.snp.leading).offset(-5)
+            $0.height.equalTo(number3.snp.height)
+            $0.width.equalTo(20)
+        }
+        
+        resultLabel.snp.makeConstraints {
+            $0.top.equalTo(number3.snp.bottom).offset(10)
+            $0.horizontalEdges.equalTo(safeArea).inset(80)
+            $0.height.equalTo(30)
+        }
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        [number1, number2, number3].forEach {
+            $0.backgroundColor = .systemGray6
+            $0.keyboardType = .numberPad
+            $0.textAlignment = .left
+        }
+        
+        plusLabel.text = "+"
+        plusLabel.font = .systemFont(ofSize: 18, weight: .semibold)
+        resultLabel.backgroundColor = .systemGray4
+    }
+    
+    func calculator() {
+        Observable.combineLatest(
+            number1.rx.text.orEmpty,
+            number2.rx.text.orEmpty,
+            number3.rx.text.orEmpty) { value1, value2, value3 -> Int in
+                return (Int(value1) ?? 0) + (Int(value2) ?? 0) + (Int(value3) ?? 0)
+            }
+            .map { $0.description }
+            .bind(to: resultLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
 }
